279a6cbe2560abcd1c037f8d2bd5d11193f5f500
André-git <andre.potthast.ap@gmail.com>
2023-06-11 08:58:43 +0200

    basic of the basic layout done

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { createTask } from "./tasks";
 
 
 // playing around testing?
-const test = (document.querySelector("#test").textContent = "index.js works");
+// const test = (document.querySelector("#test").textContent = "index.js works");
 
 let testTask=createTask("newTask","a test "," 22.02.02", "2",true)
 console.log("add To TempStorage")
